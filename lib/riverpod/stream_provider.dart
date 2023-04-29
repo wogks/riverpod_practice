289e04aacdb9670e202bff2bc6ea1d1c487d34bf
wogks <wogks27@naver.com>
2023-04-29 21:46:14 +0900

    Feat: stream provider

--- lib/riverpod/stream_provider.dart
+++ lib/riverpod/stream_provider.dart
@@ -5,7 +5,7 @@ final multipleStreamProvider = StreamProvider<List<int>>(
     for (int i = 0; i < 10; i++) {
       await Future.delayed(const Duration(seconds: 1));
 
-      yield List.generate(3, (index) => index * 1);
+      yield List.generate(3, (index) => index * i);
     }
   },
 );
--- lib/screen/stream_provider_screen.dart
+++ lib/screen/stream_provider_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:riverpodpractice/layout/default_layout.dart';
+import 'package:riverpodpractice/riverpod/stream_provider.dart';
 
-class StreamProviderScreen extends StatelessWidget {
+class StreamProviderScreen extends ConsumerWidget {
   const StreamProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(multipleStreamProvider);
     return DefaultLayout(
-      title: 'Stream Provider',
-      body: Column(
-        children: const [],
-      ),
-    );
+        title: 'Stream Provider',
+        body: Center(
+          child: state.when(
+            data: (data) {
+              return Text(data.toString());
+            },
+            error: (error, stackTrace) => Text(error.toString()),
+            loading: () => const CircularProgressIndicator(),
+          ),
+        ));
   }
 }
