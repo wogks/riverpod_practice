372c9889c873c4841fc439207e0b6e4593f8b892
wogks <wogks27@naver.com>
2023-04-30 22:42:53 +0900

    Feat: provider observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpodpractice/riverpod/provider_observer.dart';
 
 import 'package:riverpodpractice/screen/home_screen.dart';
 
 void main() {
-  runApp(const ProviderScope(
-    child: MaterialApp(
-      home: HomeScreen(),
+  runApp(
+    ProviderScope(
+      observers: [
+        Logger(),
+      ],
+      child: const MaterialApp(
+        home: HomeScreen(),
+      ),
     ),
-  ));
+  );
 }
